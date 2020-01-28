ecf6a513c9dc1c7752dd002fea16cf2a2d1c2fdb
deepakkmr0094 <deepakkmr0094@gmail.com>
2020-01-29 02:19:42 +0530

    Added additional checks to Test2

--- src/com/javaa2z/main/Test2.java
+++ src/com/javaa2z/main/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		String hungry="yes";
-		if(hungry.equals("yes"))
+		if(hungry.equals("yes") || hungry.equals('Y')  || hungry.equals('Y'))
 				System.out.println("eat something..");
 		else
 			System.out.println("Do work...");
