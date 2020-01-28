23345fc7d5530bf7c24d8d7bf05e9ba388eb1479
deepakkmr0094 <deepakkmr0094@gmail.com>
2020-01-29 02:00:06 +0530

    Commiting Test2 to BranchX

---
+++ src/com/javaa2z/main/Test2.java
@@ -0,0 +1,13 @@
+package com.javaa2z.main;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry.equals("yes"))
+				System.out.println("eat something..");
+		else
+			System.out.println("Do work...");
+	}
+
+}
